c3eca63c58d8021718839ed844f6b24feaa5c78b
98mohitkumar <98mohitkumar@gmail.com>
2022-10-30 09:07:31 +0530

    reviews formatting changes

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -7,40 +7,27 @@ import {
   ReviewsWrap
 } from './ReviewsStyles';
 import { Span } from '../MovieInfo/MovieDetailsStyles';
-import { useMemo } from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const Reviews = ({ reviews }) => {
-  const filtered = useMemo(
-    () =>
-      reviews.map((item) => ({
-        ...item,
-        content: item.content
-          .replace(/\n|\r\n|\r/g, '<br/>')
-          .replace(/(?:\*)(?:(?!\s))((?:(?!\*|\n).)+)(?:\*)/g, '<b>$1</b>')
-          .replace(/(?:_)(?:(?!\s))((?:(?!\n|_).)+)(?:_)/g, '<i>$1</i>')
-          .replace(/(?:~)(?:(?!\s))((?:(?!\n|~).)+)(?:~)/g, '<s>$1</s>')
-          .replace(/(?:--)(?:(?!\s))((?:(?!\n|--).)+)(?:--)/g, '<u>$1</u>')
-          .replace(/(?:```)(?:(?!\s))((?:(?!\n|```).)+)(?:```)/g, '<tt>$1</tt>')
-      })),
-    [reviews]
-  );
-
   return (
     <>
       <ReviewsContainer>
-        {filtered.length === 0 ? (
+        {reviews.length === 0 ? (
           <NoDataText className='fw-bold text-center my-5'>
             No Reviews Yet
           </NoDataText>
         ) : (
-          filtered.map((item) => (
+          reviews.map((item) => (
             <ReviewsWrap key={item.id}>
               <ReviewAuthorWrap>
                 <ReviewAuthorImg id={item.id} />
                 <Span>{item.author}</Span>
               </ReviewAuthorWrap>
 
-              <Review dangerouslySetInnerHTML={{ __html: item.content }} />
+              <Review>
+                <ReactMarkdown>{item.content}</ReactMarkdown>
+              </Review>
             </ReviewsWrap>
           ))
         )}
