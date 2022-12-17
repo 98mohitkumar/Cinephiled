2220d4eacd452a1664df7d966c4bb79d8eaad79f
98mohitkumar <98mohitkumar@gmail.com>
2022-12-17 18:11:37 +0530

    added mediaCard component

---
+++ src/components/ProfilePage/MediaCard.js
@@ -0,0 +1,75 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiFillStar } from 'react-icons/ai';
+import {
+  CardImg,
+  CardInfo,
+  Cards,
+  InfoTitle,
+  Rating,
+  ReleaseDate
+} from '../Popular/PopularStyles';
+import { RatingOverlay } from './ProfilePageStyles';
+
+const MediaCard = ({ data, link, children, rating, recommendation }) => {
+  return (
+    <Cards>
+      <motion.div
+        whileHover={{
+          scale: 1.05,
+          transition: { duration: 0.1 }
+        }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <Link
+          href={`/${link}/${data?.id}-${(data?.title || data?.name).replace(
+            /[' ', '/']/g,
+            '-'
+          )}`}
+          passHref
+          scroll={false}
+        >
+          <a>
+            <CardImg className='d-flex justify-content-end'>
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
+                alt='movie-poster'
+                layout='fill'
+                objectFit='cover'
+                className='poster'
+              />
+
+              {rating && (
+                <RatingOverlay>
+                  <AiFillStar size='14px' />
+                  <p className='m-0 fw-semibold'>{rating}</p>
+                </RatingOverlay>
+              )}
+
+              {recommendation && (
+                <Rating className='d-flex justify-content-center align-items-center'>
+                  {data.vote_average === 0
+                    ? 'NR'
+                    : data.vote_average.toFixed(1)}
+                </Rating>
+              )}
+            </CardImg>
+          </a>
+        </Link>
+      </motion.div>
+      {recommendation ? (
+        <CardInfo>
+          <InfoTitle>{data?.title || data?.name}</InfoTitle>
+          <ReleaseDate>
+            {data?.release_date || data?.first_air_date}
+          </ReleaseDate>
+        </CardInfo>
+      ) : (
+        <div style={{ paddingTop: '20px' }}>{children}</div>
+      )}
+    </Cards>
+  );
+};
+
+export default MediaCard;
