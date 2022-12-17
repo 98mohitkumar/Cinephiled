59875ed99c472f51ebb853e49321817c05c1a4f4
98mohitkumar <98mohitkumar@gmail.com>
2022-12-17 17:19:12 +0530

    switch to native styledComponents compiler

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development'
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['image.tmdb.org'],
-    unoptimized: true,
+    unoptimized: true
   },
+  compiler: {
+    styledComponents: true
+  }
 });
