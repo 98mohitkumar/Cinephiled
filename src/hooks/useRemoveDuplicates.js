cbb0e27defd8b8c8452d4dff7942b0d69f8ba7cd
98mohitkumar <98mohitkumar@gmail.com>
2023-02-11 18:06:26 +0530

    added remove duplicates hook

---
+++ src/hooks/useRemoveDuplicates.js
@@ -0,0 +1,18 @@
+const useRemoveDuplicates = (items) => {
+  let filtered = [];
+
+  const cleanedItems = items
+    .map((item) => {
+      if (filtered.includes(item.id)) {
+        return { duplicate: true };
+      } else {
+        filtered.push(item.id);
+        return item;
+      }
+    })
+    .filter((item) => !item?.duplicate);
+
+  return { cleanedItems };
+};
+
+export default useRemoveDuplicates;
