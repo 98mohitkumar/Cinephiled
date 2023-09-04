4c6ce65a57c7c54290a1072fcf4c41edd105cc29
98mohitkumar <98mohitkumar@gmail.com>
2023-09-04 19:35:57 +0530

    remove reference to the file after initiating download

--- src/components/DownloadMediaButton/DownloadMediaButton.js
+++ src/components/DownloadMediaButton/DownloadMediaButton.js
@@ -1,20 +1,24 @@
-import { AiOutlineDownload } from 'react-icons/ai';
-import { MediaDownloadButton } from './DownloadMediaButtonStyles';
+import { AiOutlineDownload } from "react-icons/ai";
+import { MediaDownloadButton } from "./DownloadMediaButtonStyles";
 
 export const DownloadMediaButton = ({ item }) => {
   const download = (e, id) => {
     fetch(e.target.attributes.href.value, {
-      method: 'GET',
+      method: "GET",
       headers: {}
     })
       .then((response) => {
         response.arrayBuffer().then(function (buffer) {
           const url = window.URL.createObjectURL(new Blob([buffer]));
-          const link = document.createElement('a');
+          const link = document.createElement("a");
           link.href = url;
-          link.setAttribute('download', `${id}.jpg`); //or any other extension
+          link.setAttribute("download", `${id}.jpg`); //or any other extension
           document.body.appendChild(link);
           link.click();
+
+          setTimeout(() => {
+            window.URL.revokeObjectURL(url);
+          }, 100);
         });
       })
       .catch((err) => {
@@ -26,8 +30,7 @@ export const DownloadMediaButton = ({ item }) => {
     <MediaDownloadButton
       role='button'
       href={`https://image.tmdb.org/t/p/original${item}`}
-      onClick={(e) => download(e, item)}
-    >
+      onClick={(e) => download(e, item)}>
       <AiOutlineDownload color='#414141' />
     </MediaDownloadButton>
   );
