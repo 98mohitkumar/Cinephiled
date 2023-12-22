ecc48943e0578681ce8285a11a8c04c930423cba
98mohitkumar <98mohitkumar@gmail.com>
2023-12-22 20:30:30 +0530

    gradient color updates

--- src/components/DominantColor/DominantColorStyles.js
+++ src/components/DominantColor/DominantColorStyles.js
@@ -4,15 +4,15 @@ export const Colorful = styled.div`
   inset: 0;
   background-image: linear-gradient(90deg, #ccbdb4, #392a28d6);
   background-image: ${({ palette }) =>
-    `linear-gradient(90deg, ${palette?.vibrant + "d6"} 40%, ${palette?.darkVibrant + "8e"})`};
+    `linear-gradient(90deg, ${palette?.[0]}, ${palette?.[1] + "8e"})`};
 
   &.tint {
     opacity: 0.4;
     background-image: ${({ flip }) =>
       `linear-gradient(${flip ? "180deg" : "0deg"}, #ccbdb463, #392a28d6, #121212)`};
     background-image: ${({ palette, flip }) =>
-      `linear-gradient(${flip ? "180deg" : "0deg"}, ${palette.vibrant + "94"}, ${
-        palette.darkVibrant + "8e"
+      `linear-gradient(${flip ? "180deg" : "0deg"}, ${palette?.[0] + "94"}, ${
+        palette?.[1] + "8e"
       }, #121212)`};
   }
 `;
