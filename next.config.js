4ee319ac7101e0b7d4ccb6e45559de3b348b1cf8
98mohitkumar <98mohitkumar@gmail.com>
2024-05-29 21:13:19 +0530

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,73 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === "development"
 });
 
-module.exports = withPWA({
+const cspHeader = `
+    default-src *;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.clarity.ms/;
+    style-src 'self' 'unsafe-inline';
+    img-src * data:;
+    font-src 'self' data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+  `;
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, "")
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin"
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          }
+        ]
+      }
+    ];
+  },
   reactStrictMode: true,
   images: {
-    domains: ['image.tmdb.org'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/"
+      }
+    ],
     unoptimized: true
   },
   compiler: {
     styledComponents: true
   }
-});
+};
+
+module.exports = withPWA(nextConfig);
