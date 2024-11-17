33ba922a7e1e4d0bbe2d85667358d26841613368
98mohitkumar <98mohitkumar@gmail.com>
2024-11-17 12:55:29 +0530

    updated button config

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -4,7 +4,7 @@ import { cn, matches } from "utils/helper";
 
 type ButtonProps = ComponentPropsWithoutRef<"button"> &
   Omit<MotionProps, "variants"> & {
-    variant?: "primary" | "secondary";
+    variant?: "primary" | "secondary" | "outline";
     size?: "small" | "default" | "large";
     weight?: "normal" | "medium" | "semibold" | "bold";
   };
@@ -17,14 +17,17 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 0.95 }}
         className={cn(
+          "grid place-items-center",
+          "transition-colors ease-in-out disabled:pointer-events-none disabled:cursor-not-allowed",
           {
             "bg-neutral-200 text-black": matches(variant, "primary"),
-            "bg-neutral-700 text-white": matches(variant, "secondary")
+            "bg-neutral-700 text-white": matches(variant, "secondary"),
+            "border border-neutral-600 bg-black text-white": matches(variant, "outline")
           },
           {
-            "rounded-md px-12 py-6 text-small": matches(size, "small"),
-            "rounded-lg px-16 py-8 text-p": matches(size, "default"),
-            "rounded-xl px-20 py-10 text-h6": matches(size, "large")
+            "h-9 rounded-md px-12 text-small": matches(size, "small"),
+            "h-10 rounded-lg px-16 text-p": matches(size, "default"),
+            "h-11 rounded-lg px-24 text-p": matches(size, "large")
           },
           {
             "font-normal": matches(weight, "normal"),
