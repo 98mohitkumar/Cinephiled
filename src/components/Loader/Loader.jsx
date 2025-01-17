e6d8c23771e6121c9877a888244cbd1fecdb76cf
98mohitkumar <98mohitkumar@gmail.com>
2025-01-17 22:31:02 +0530

    updated loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,26 @@
 import { loaderStyles } from "./LoaderStyles";
 
-// https://codepen.io/JMChristensen/pen/GRLLVpa
+// https://uiverse.io/StealthWorm/chatty-zebra-11
 export const Loader = (props) => {
   return (
     <div css={loaderStyles} {...props}>
-      <div className='stack'>
-        <div className='blobs'>
-          <span className='blob'></span>
-          <span className='blob'></span>
-          <span className='blob'></span>
-          <span className='blob'></span>
+      <div className='loader'>
+        <div className='carousel'>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <div key={index} className='love icon' />
+          ))}
+        </div>
+
+        <div className='carousel'>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <div key={index} className='death icon' />
+          ))}
+        </div>
+
+        <div className='carousel'>
+          {Array.from({ length: 7 }).map((_, index) => (
+            <div key={index} className='robots icon' />
+          ))}
         </div>
       </div>
     </div>
@@ -17,12 +28,6 @@ export const Loader = (props) => {
 };
 
 // redundant check (<Loading />) and .small class in above component
-const Loading = () => {
-  return (
-    <div className='grid min-h-[45vh] place-items-center'>
-      <Loader className='small' />
-    </div>
-  );
-};
+const Loading = () => null;
 
 export default Loading;
--- src/components/Loader/LoaderStyles.js
+++ src/components/Loader/LoaderStyles.js
@@ -1,11 +1,53 @@
-import { css } from "styled-components";
+import { css, keyframes } from "styled-components";
 
 import { theme } from "theme/theme";
 
+const scrollUp = keyframes`
+  0% {
+    transform: translateY(-88%);
+  }
+
+  40% {
+    transform: translateY(-5%);
+  }
+
+  80% {
+    transform: translateY(-88%);
+  }
+
+  100% {
+    transform: translateY(-88%);
+  }
+`;
+
+const scrollDown = keyframes`
+  0% {
+    transform: translateY(-14.7%);
+  }
+
+  50% {
+    transform: translateY(-88%);
+  }
+
+  80% {
+    transform: translateY(-14.7%);
+  }
+
+  100% {
+    transform: translateY(-14.7%);
+  }
+`;
+
+const rotation = keyframes`
+    20%, 100% {
+      transform: rotate(180deg);
+    }
+`;
+
 export const loaderStyles = css`
   position: fixed;
-  z-index: 9999;
-  background: #121212;
+  z-index: ${theme.zIndex.modal + 1000};
+  background: ${theme.colors.black};
   inset: 0;
   margin: auto;
   display: grid;
@@ -14,100 +56,86 @@ export const loaderStyles = css`
   &.small {
     position: static;
     background: transparent;
-
-    .blob {
-      --border-width: 1vmin;
-      height: 15vmin;
-    }
   }
 
-  --aqua: #7fdbff;
-  --blue: #0074d9;
-  --navy: #001f3f;
-  --teal: #39cccc;
-  --green: #2ecc40;
-  --olive: #3d9970;
-  --lime: #01ff70;
-
-  --yellow: #ffdc00;
-  --orange: #ff851b;
-  --red: #ff4136;
-  --fuchsia: #f012be;
-  --purple: #b10dc9;
-  --maroon: #85144b;
-
-  --white: #ffffff;
-  --silver: #dddddd;
-  --gray: #aaaaaa;
-  --black: #111111;
-
-  .stack {
+  .loader {
     display: grid;
-    grid-template-areas: "stack";
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+    height: 56px;
+    width: 200px;
+    overflow: hidden;
   }
 
-  .blobs {
-    display: grid;
-    grid-template-areas: "stack";
-    position: relative;
-    animation: spin infinite 1.75s ${theme.transitionTimings["ease-in-out-back"]};
-    grid-area: stack;
+  .carousel {
+    width: 100%;
+    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    animation-delay: 2s;
   }
 
-  .blob {
-    --border-radius: 115% 140% 145% 110% / 125% 140% 110% 125%;
-    --border-width: 2.25vmin;
-
+  .icon {
+    display: flex;
+    width: 100%;
     aspect-ratio: 1;
-    display: block;
-    grid-area: stack;
-    background-size: calc(100% + var(--border-width) * 2);
-    background-repeat: no-repeat;
-    background-position: center;
-    border: var(--border-width) solid transparent;
-    border-radius: var(--border-radius, 50%);
-    mask-image: linear-gradient(transparent, transparent), linear-gradient(black, white);
-    mask-clip: padding-box, border-box;
-    mask-composite: intersect;
-    mix-blend-mode: screen;
-    height: 35vmin;
-    filter: blur(1vmin);
-
-    &:nth-child(1) {
-      background-color: var(--blue);
-      background-image: linear-gradient(var(--blue), var(--teal), var(--blue));
-      rotate: 30deg;
-      scale: 1.03;
-    }
-
-    &:nth-child(2) {
-      background-color: var(--red);
-      background-image: linear-gradient(var(--red), var(--orange), var(--red));
-      rotate: 60deg;
-      scale: 0.95;
-    }
-
-    &:nth-child(3) {
-      background-color: var(--olive);
-      background-image: linear-gradient(var(--olive), var(--lime), var(--olive));
-      rotate: 90deg;
-      scale: 0.97;
-    }
-
-    &:nth-child(4) {
-      background-color: var(--purple);
-      background-image: linear-gradient(var(--purple), var(--maroon), var(--purple));
-      rotate: 120deg;
-      scale: 1.02;
-    }
   }
 
-  @keyframes spin {
-    from {
-      rotate: 0deg;
-    }
-    to {
-      rotate: 360deg;
-    }
+  .loader .carousel:nth-child(1) {
+    animation: ${scrollUp} 2.75s infinite ease-in-out;
+  }
+
+  .loader .carousel:nth-child(2) {
+    animation: ${scrollDown} 2.75s infinite ease-in-out;
+  }
+
+  .loader .carousel:nth-child(3) {
+    animation: ${scrollDown} 2.75s infinite ease-in-out;
+  }
+
+  .love {
+    background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15"><path d="M13.91,6.75c-1.17,2.25-4.3,5.31-6.07,6.94c-0.1903,0.1718-0.4797,0.1718-0.67,0C5.39,12.06,2.26,9,1.09,6.75C-1.48,1.8,5-1.5,7.5,3.45C10-1.5,16.48,1.8,13.91,6.75z" fill="white"/></svg>')
+      no-repeat center/contain;
+    animation-delay: 2s;
+  }
+
+  .death {
+    align-items: center;
+    justify-content: center;
+    animation: ${rotation} 2.25s 1s infinite ease-in-out;
+  }
+
+  .death:after {
+    content: "";
+    height: 63px;
+    position: absolute;
+    border-left: 12px solid ${theme.colors.white};
+    transform: rotate(45deg);
+    border-radius: 8px;
+  }
+
+  .death:before {
+    content: "";
+    height: 60px;
+    position: absolute;
+    border-left: 12px solid ${theme.colors.white};
+    transform: rotate(-45deg);
+  }
+
+  .robots {
+    justify-content: space-between;
+    background-color: ${theme.colors.white};
+    border-radius: 0 8px 8px;
+    padding: 8px;
+  }
+
+  .robots::after,
+  .robots::before {
+    content: "";
+    width: 12px;
+    height: 12px;
+    background-color: ${theme.colors.black};
+    border-radius: 50%;
   }
 `;
