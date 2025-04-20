642ee166cbcd0d2546775aef9a4aa6e73128bba3
98mohitkumar <98mohitkumar@gmail.com>
2025-04-20 17:08:07 +0530

    some styling fixes

--- src/components/Shared/DominantColor/DominantColorStyles.js
+++ src/components/Shared/DominantColor/DominantColorStyles.js
@@ -1,11 +1,13 @@
 import { css } from "styled-components";
 
+import { theme } from "theme/theme";
+
 export const colorOverlayStyles = css`
   background-image: linear-gradient(90deg, #ccbdb4, #392a28d6);
   background-image: ${({ $palette, $angle }) => `linear-gradient(${$angle || "90deg"}, ${$palette?.[0]}, ${$palette?.[1] + "8e"})`};
 `;
 
 export const tintedColorOverlayStyles = css`
-  background-image: ${({ $angle }) => `linear-gradient(${$angle || "180deg"}, #ccbdb463, #392a28d6, #121212)`};
-  background-image: ${({ $palette, $angle }) => `linear-gradient(${$angle || "180deg"}, ${$palette?.[0] + "94"}, ${$palette?.[1] + "8e"}, #121212)`};
+  background-image: ${({ $angle }) => `linear-gradient(${$angle || "180deg"}, #ccbdb463, #392a28d6, ${theme.colors.black})`};
+  background-image: ${({ $palette, $angle }) => `linear-gradient(${$angle || "180deg"}, ${$palette?.[0] + "94"}, ${theme.colors.black})`};
 `;
